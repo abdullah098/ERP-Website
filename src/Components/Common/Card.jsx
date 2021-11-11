41bcca7394a55deb9f06e53e908a201182ea6761
Abdullah Iqbal <ai3449713@gmail.com>
2021-11-11 09:44:06 +0500

    solection section and solution cards

---
+++ src/Components/Common/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ReactComponent as ArrowRight} from '../../Assets/ArrowRight.svg';
+
+const Card = (probs) => {
+    return ( 
+        <div className="primary-card">
+            <div className="card-image">
+                <img className="rounded-circle" src={probs.image} alt="" />
+            </div>
+            <div className="card-content text-center">
+                <h4 className="fw-bold"> {probs.title} </h4>
+                <p>{probs.description}</p>
+            </div>
+            <div className="card-f text-center">
+                <a href={probs.link}>Learn More <span>{<ArrowRight />}</span></a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Card;
--- src/Components/HomeComponent/Solution.jsx
+++ src/Components/HomeComponent/Solution.jsx
@@ -1,12 +1,53 @@
 import React from 'react';
+import Card from './../Common/Card';
+import Store from '../../Assets/store.svg';
+import Boxes from '../../Assets/boxes.svg';
+import Sketch from '../../Assets/sketch (1).svg';
 
 const Solution = () => {
+
+    const cards = [
+        {
+            title: 'Construction Industries', 
+            description: 'This covers Industries like construction,retail stores and gives you ease to get all records This covers Industries like construction,retail stores and gives you ease to get all records This covers Industries like construction,retail stores and gives you ease to get all records',
+            image: Store,
+            link: '/'
+        },
+        {
+            title: 'Construction Retail Stores', 
+            description: 'This covers Industries like construction,retail stores and gives you ease to get all records This covers Industries like construction,retail stores and gives you ease to get all records This covers Industries like construction,retail stores and gives you ease to get all records',
+            image: Sketch,
+            link: '/'
+        },
+        {
+            title: 'Construction Warehouses', 
+            description: 'This covers Industries like construction,retail stores and gives you ease to get all records This covers Industries like construction,retail stores and gives you ease to get all records This covers Industries like construction,retail stores and gives you ease to get all records',
+            image: Boxes,
+            link: '/'
+        }
+    ]
+
+
     return (
         <>
             <div className="solution border border-secondary">
                 <div className="solution-content border border-danger">
                     <h1 className="fw-bold m-0">We Provide Solution for Different Businesses</h1>
                     <p >This is the best digital tool for all your businesses which you operate online it covers Industries like construction, retail stores and gives you ease to get all the records in one place without worrying about.</p>
+
+                    <div className="row">
+                        {cards.map((card, index) => (
+                            <div className="col-4">
+                                <Card 
+                                    title={card.title}
+                                    description={card.description}
+                                    image={card.image}
+                                    link={card.link}
+
+                                 />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>

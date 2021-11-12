a37622a31f2f9f3ccf8f4400d62b642eb9ba1c1d
Abdullah Iqbal <ai3449713@gmail.com>
2021-11-12 14:26:14 +0500

    request a demo nav model complete

--- src/Layouts/NavBar.jsx
+++ src/Layouts/NavBar.jsx
@@ -1,8 +1,40 @@
-import React from 'react';
+
 import { NavLink } from 'react-router-dom';
 import logo from '../Assets/Group 2070.svg';
+import React, { useState } from 'react';
+import { Modal, Button } from 'antd';
+import RequestADemo from './RequestADemo';
 
 const NavBar = () => {
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
+    const handleOk = () => {
+        setIsModalVisible(false);
+        this.setState({ loading: true });
+        setTimeout(() => {
+            this.setState({ loading: false, visible: false });
+        }, 3000);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
+
+    
+
+
+
+
+
+
+
+
+
     return (
         <>
             <nav className="nav nav_style d-flex justify-content-between">
@@ -13,30 +45,40 @@ const NavBar = () => {
 
 
                 <div className=" d-flex align-items-center">
-                    <div>
-                        <NavLink to="/home">
+                    {/* <div> */}
+
+                    <NavLink to="/home">Home</NavLink>
+                        {/* <NavLink to="/home">
                             Home
-                        </NavLink>
-                    </div>
-                    <div>
+                        </NavLink> */}
+                    {/* </div> */}
+                    {/* <div> */}
                         <NavLink to="/aboutUs">
                             About Us
                         </NavLink>
-                    </div>
-                    <div>
+                    {/* </div> */}
+                    {/* <div> */}
                         <a href="a">Feature</a>
-                    </div>
-                    <div>
+                    {/* </div> */}
+                    {/* <div> */}
                         <a href="a">Pricing Plan</a>
-                    </div>
-                    <div>
+                    {/* </div> */}
+                    {/* <div> */}
                         <a href="a">Contact Us</a>
-                    </div>
+                    {/* </div> */}
                 </div>
 
 
                 <div>
-                    <button className="btn bg-transparent text-white border border-white">Request Demo</button>
+
+                    <Button type="btn bg-transparent text-white border border-white" onClick={showModal}>
+                        Request Demo
+                    </Button>
+                    <Modal visible={isModalVisible}  onCancel={handleCancel} footer={[
+                        
+                    ]}>
+                        <RequestADemo />
+                    </Modal>
                 </div>
             </nav>
         </>
---
+++ src/Layouts/RequestADemo.jsx
@@ -0,0 +1,142 @@
+import React, { useState } from 'react';
+import { Select } from 'antd';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
+import { Input } from 'antd';
+
+const RequestADemo = () => {
+
+    const cointries = ['Pakistan', 'India', 'Saudia Arab', 'United States', 'England'];
+
+    const [phone, setPhone] = useState();
+
+    const { Option } = Select;
+    console.log(Select);
+
+    function onChange(value) {
+        // console.log(`selected ${value}`);
+    }
+
+    function onBlur() {
+        // console.log('blur');
+    }
+
+    function onFocus() {
+        // console.log('focus');
+    }
+
+    function onSearch(val) {
+        // console.log('search:', val);
+    }
+
+
+
+    const handlePhoneChange = (phone) => {
+        if (phone === "") setPhone(null);
+        else setPhone(phone);
+    }
+
+    return (
+        <>
+            <div className="request-demo">
+                <h4 className="fw-bold" >Request Demo Form</h4>
+                <p>For the prism demo fill the details below to get your demo.</p>
+
+                <form className="request-demo-form">
+                    <div className="form-group">
+
+                        <div className="d-flex">
+                            <div className="flex-colum">
+                                <label htmlFor="name">Name</label>
+                                <Input placeholder="Enter your name" />
+                                {/* <input type="text" className="form-control" id="name" placeholder="Enter your name" /> */}
+                            </div>
+
+                            <div className="flex-colum">
+                                <label htmlFor="email">Email</label>
+                                <Input placeholder="Enter your Email" />
+                                {/* <input type="email" className="form-control" id="email" placeholder="Enter your email" /> */}
+                            </div>
+
+                        </div>
+
+                        <div className="d-flex">
+                            <div className="flex-colum">
+                                <label htmlFor="phone_number">Mobile Number</label>
+                                <PhoneInput
+                                    country={''}
+                                    value={phone}
+                                    onChange={handlePhoneChange}
+                                />
+                                {/* <input type="text" className="form-control" id="phone_number" placeholder="" /> */}
+
+
+                            </div>
+                            <div className="flex-colum">
+                                <label htmlFor="Country">Country</label>
+                                <br />
+                                <Select
+                                    showSearch
+                                    placeholder="Select a Country"
+                                    optionFilterProp="children"
+                                    onChange={onChange}
+                                    onFocus={onFocus}
+                                    onBlur={onBlur}
+                                    onSearch={onSearch}
+                                    filterOption={(input, option) =>
+                                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                                    }
+                                >
+
+                                    {cointries.map((country, index) => (
+                                        <Option key={index} value={country}>{country}</Option>
+                                    ))}
+                                </Select>
+
+
+                                {/* <input type="text" className="form-control"  id="Country" placeholder="Enter your Country" list="mylist"/>
+                                <datalist className="bg-white" id="mylist">
+
+                                    {cointries.map((country, index) => (
+                                        <option key={index} value={country} />
+                                    ))}
+                                    
+                                </datalist> */}
+
+                            </div>
+                        </div>
+
+                        <div className="d-flex">
+                            <div className="flex-colum">
+                                <label htmlFor="phone">Campany</label>
+                                <Input placeholder="Company" />
+                                {/* <input type="text" className="form-control" id="phone" placeholder="Enter your phone" /> */}
+
+
+                            </div>
+                            <div className="flex-colum">
+                                <label htmlFor="company">Number of Users</label>
+                                <Input placeholder="# of Users" />
+                                {/* <input type="text" className="form-control" id="company" placeholder="Enter your company" /> */}
+                            </div>
+                        </div>
+
+
+
+                        <div className="reset-form float-end border" >
+                            <a href="/">Reset from </a>
+                        </div>
+
+
+                        <button type="submit" className="btn btn-primary submit-btn">Submit</button>
+
+
+
+                    </div>
+                </form>
+            </div>
+        </>
+    );
+}
+
+export default RequestADemo;
